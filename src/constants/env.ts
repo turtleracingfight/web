e1c921ca6e86fe173cc0e90c7125672464efad37
olegmaslovsky <o.maslovsky@emcd.io>
2024-12-01 13:15:27 +0300

    publish app

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,4 @@
-const MANIFEST_TON = import.meta.env.VITE_MANIFEST_TON || "";
+const MANIFEST_TON = import.meta.env.VITE_MANIFEST_TON || "https://hubaxis.github.io/web/turtle.json";
 const REQUEST_TO_BALANCE =
   "https://testnet.toncenter.com/api/v2/getAddressBalance?address=";
 
