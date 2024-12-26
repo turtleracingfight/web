e1e28d0fd75f7ea9371ab705886dac7bc7acf0f7
olegmaslovsky <o.maslovsky@emcd.io>
2024-12-26 18:08:29 +0300

    Fix duration

--- src/hooks/useControlCenter.tsx
+++ src/hooks/useControlCenter.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import { useStoreContact } from "../store/store-contract.ts";
 
 let time = 0;
-const CONTRACT = "kQDq2xl1rbIIFR_hHxp5M8FFIwZsE_UOUZt7mKksMVaumIvW";
+const CONTRACT = "kQCzxJ9t8ncAIf8lU6wMWieawu6fqN1U7EPkxqbSclMpBE0m";
 export const useControlCenter = () => {
   const [isLoaded, setIsLoaded] = useState<boolean>(true);
   const { address, sender, client, setOptions } = useAccount();
