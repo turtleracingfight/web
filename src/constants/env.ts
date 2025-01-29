87797dfdaab36be5fd88fad737d9b4bdc03898e0
olegmaslovsky <o.maslovsky@emcd.io>
2025-01-29 09:04:22 +0300

    Fix balance

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -2,6 +2,6 @@ const MANIFEST_TON =
   import.meta.env.VITE_MANIFEST_TON ||
   "https://turtleracingfight.github.io/web/turtle.json";
 const REQUEST_TO_BALANCE =
-  "https://testnet.toncenter.com/api/v2/getAddressBalance?address=";
+  "https://toncenter.com/api/v2/getAddressBalance?address=";
 
 export { MANIFEST_TON, REQUEST_TO_BALANCE };
