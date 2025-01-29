c6136b93f6ba2817d341dcaa3fdfd1e8cd83dcdb
olegmaslovsky <o.maslovsky@emcd.io>
2025-01-29 12:39:40 +0300

    Add analytics

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,12 @@ import { THEME } from "@tonconnect/ui";
 import { MANIFEST_TON } from "./constants/env.ts";
 import "@twa-dev/sdk";
 import { StrictMode } from "react";
+import telegramAnalytics from '@telegram-apps/analytics';
+
+telegramAnalytics.init({
+  token: 'eyJhcHBfbmFtZSI6IlR1cnRsZUZpZ2h0Qm90IiwiYXBwX3VybCI6Imh0dHBzOi8vdC5tZS9UdXJ0bGVGaWdodEJvdCIsImFwcF9kb21haW4iOiJodHRwczovL3R1cnRsZXJhY2luZ2ZpZ2h0LmdpdGh1Yi5pby93ZWIifQ==!gZWBB7HXHO2gPSCifk6298B7ag7CJkPfyFrwedXal4s=',
+  appName: 'TurtleFightBot',
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
