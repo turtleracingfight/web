bb0bec540ce4f7dc62351ab0249e127a97997a7c
Vlas <Maskalenchik>
2025-01-29 18:06:33 +0300

    some fixed

--- src/pages/list-turtles.tsx
+++ src/pages/list-turtles.tsx
@@ -1,12 +1,12 @@
-import styles from "../styles/pages/list-turtles.module.scss";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { BtnCommon } from "../components/buttons.tsx";
+import { LANGS } from "../constants/langs.ts";
 import { CURRENCY, TURTLES_LINKS } from "../constants/links.ts";
 import { ROUTES } from "../constants/route.tsx";
-import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { LANGS } from "../constants/langs.ts";
 import { useLang } from "../hooks/useLang.tsx";
 import { useStoreContact } from "../store/store-contract.ts";
+import styles from "../styles/pages/list-turtles.module.scss";
 import { countTotalTon } from "../utils/usefulFunc.ts";
 
 export const ListTurtles = () => {
@@ -14,11 +14,18 @@ export const ListTurtles = () => {
   const { lang } = useLang();
   const [bet, setBet] = useState<{ [key: string]: bigint | string }>({});
   const requestGetData = useStoreContact(state => state.requestGetData);
+  const [isZIndex, setIsZIndex] = useState(false);
 
   const handlerMakeBet = (id: number) => {
     window.sessionStorage.setItem("prev-page", ROUTES.listTurtles);
     navigate(`${ROUTES.makeBet}/${id + 1}`);
   };
+
+  const handleScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    const isBottom = scrollTop + clientHeight >= scrollHeight - (scrollHeight * 0.05);
+    setIsZIndex(() => isBottom);
+  }
 
   useEffect(() => {
     (async () => {
@@ -27,8 +34,13 @@ export const ListTurtles = () => {
     })();
   }, []);
 
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ zIndex: isZIndex ? 11 : 1, height: 'auto', backgroundColor: 'black' }}>
       {Object.values(TURTLES_LINKS).map(el => {
         let betTon = countTotalTon(bet[`me${el.id + 1}`]) || 0;
         return (
